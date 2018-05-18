1a24cdb66d0c98df67a4c67f3ab401ee4a2c82b9
Lanen,Bram B. van <b.vanlanen@student.fontys.nl>
2018-05-18 02:47:52 +0200

    added mainscreen etc

---
+++ Assets/GoogleARCore/Examples/HelloAR/Scenes/ButtonManager.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonManager : MonoBehaviour {
+    
+
+	public void test(int saldo)
+    {
+        PlayerPrefs.SetInt("saldo", saldo);
+        SceneManager.LoadScene("tempscene");
+        Debug.Log(saldo);
+    }
+}
